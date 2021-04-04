0ae2d47c0f9363e97d489a5b575bfb3d45202dd0
Hala <sebbah.hala@gmail.com>
2021-04-04 01:43:35 +0100

    added ed and exp

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Experience from './components/Experience'
+import Studies from './components/Studies'
+
+
+
+function App() {
+  return (
+    <>
+      <Experience /> 
+      <Studies /> 
+    </> 
+  );
+}
+
+export default App;
---
+++ src/components/Studies.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+
+
+function Studies() {
+    
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const reponse = await fetch("../data/experience.json")  
+    const newJobs = await reponse.json()
+
+    fetch('data/studies.json'
+      ,{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      } 
+      ).then(function(response){
+          //console.log(response)
+          return response.json();
+        })
+        .then(function(myJson) {
+          setJobs(myJson)
+          console.log(myJson);
+          setLoading(false)
+      });
+    
+  }
+
+
+  
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+
+  
+  const { school, dates, duties, title } = jobs[value]
+
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Studies</h2> 
+        
+        <div className="underline"></div>
+      </div>
+      
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.school}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{school}</h3>
+          <h4>{title}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      
+      
+    </section>
+  )
+}
+
+export default Studies ;
